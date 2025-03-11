013e79d69d8e8757b486bf3df556c9f5c731cc33
aleols <32406373+aleols@users.noreply.github.com>
2025-03-11 01:20:19 +0100

    Create package.js

---
+++ package.js
@@ -0,0 +1,14 @@
+{
+  "name": "cycling-races-backend",
+  "version": "1.0.0",
+  "description": "Backend for managing cycling races",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.17.1",
+    "sqlite3": "^5.1.0",
+    "cors": "^2.8.5"
+  }
+}
